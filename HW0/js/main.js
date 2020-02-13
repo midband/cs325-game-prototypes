cf0c9820aff702fdcd916f1a096fdc4063c2f13c
midband <60305021+midband@users.noreply.github.com>
2020-02-12 22:46:13 -0500

    phaser-example -> game

--- HW0/js/main.js
+++ HW0/js/main.js
@@ -13,7 +13,7 @@ window.onload = function() {
         }
     };
     
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
     function preload() {
 
