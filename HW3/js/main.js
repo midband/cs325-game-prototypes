9ecb7def258cc8d25b31c3263ea0f00b6ea1e00f
George Wright <60305021+midband@users.noreply.github.com>
2020-02-26 22:08:33 -0500

    debug text

--- HW3/js/main.js
+++ HW3/js/main.js
@@ -6,6 +6,9 @@ window.onload = function() {
 	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
 	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
 
+	render() {
+    this.game.debug.text(`Debugging Phaser ${Phaser.VERSION}`, 20, 20, 'yellow', 'Segoe UI');
+}
 	//	Add the States your game has.
 	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
 	
