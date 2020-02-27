16f736f1bdd176558c8a5c09fb91a0d282bbc857
George Wright <60305021+midband@users.noreply.github.com>
2020-02-26 20:08:08 -0500

    Starstruck copy

--- HW3/js/Game.js
+++ HW3/js/Game.js
@@ -3,7 +3,16 @@
 GameStates.makeGame = function( game, shared ) {
     // Create your own variables.
     var bouncy = null;
-    
+    var map;
+    var tileset;
+    var layer;
+    var player;
+    var facing = 'left';
+    var jumpTimer = 0;
+    var cursors;
+    var jumpButton;
+    var bg;
+
     function quitGame() {
 
         //  Here you should destroy anything you no longer need.
@@ -30,40 +39,95 @@ GameStates.makeGame = function( game, shared ) {
     
         create: function () {
     
-            //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-            
-            // Create a sprite at the center of the screen using the 'logo' image.
-            bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-            // Anchor the sprite at its center, as opposed to its top-left corner.
-            // so it will be truly centered.
-            bouncy.anchor.setTo( 0.5, 0.5 );
-            
-            // Turn on the arcade physics engine for this sprite.
-            game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-            // Make it bounce off of the world bounds.
-            bouncy.body.collideWorldBounds = true;
-            
-            // Add some text using a CSS style.
-            // Center it in X, and position its top 15 pixels from the top of the world.
-            var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-            var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-            text.anchor.setTo( 0.5, 0.0 );
-            
-            // When you click on the sprite, you go back to the MainMenu.
-            bouncy.inputEnabled = true;
-            bouncy.events.onInputDown.add( function() { quitGame(); }, this );
+            game.physics.startSystem(Phaser.Physics.ARCADE);
+
+            game.stage.backgroundColor = '#000000';
+
+            bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+            bg.fixedToCamera = true;
+
+            map = game.add.tilemap('level1');
+
+            map.addTilesetImage('tiles-1');
+
+            map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+
+            layer = map.createLayer('Tile Layer 1');
+
+            //  Un-comment this on to see the collision tiles
+            // layer.debug = true;
+
+            layer.resizeWorld();
+
+            game.physics.arcade.gravity.y = 250;
+
+            player = game.add.sprite(32, 32, 'dude');
+            game.physics.enable(player, Phaser.Physics.ARCADE);
+
+            player.body.bounce.y = 0.2;
+            player.body.collideWorldBounds = true;
+            player.body.setSize(20, 32, 5, 16);
+
+            player.animations.add('left', [0, 1, 2, 3], 10, true);
+            player.animations.add('turn', [4], 20, true);
+            player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+            game.camera.follow(player);
+
+            cursors = game.input.keyboard.createCursorKeys();
+            jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
         },
     
         update: function () {
     
-            //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-            
-            // Accelerate the 'logo' sprite towards the cursor,
-            // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-            // in X or Y.
-            // This function returns the rotation angle that makes it visually match its
-            // new trajectory.
-            bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
-        }
+            game.physics.arcade.collide(player, layer);
+
+            player.body.velocity.x = 0;
+
+            if (cursors.left.isDown)
+            {
+                player.body.velocity.x = -150;
+
+                if (facing != 'left')
+                {
+                    player.animations.play('left');
+                    facing = 'left';
+                }
+            }
+            else if (cursors.right.isDown)
+            {
+                player.body.velocity.x = 150;
+
+                if (facing != 'right')
+                {
+                    player.animations.play('right');
+                    facing = 'right';
+                }
+            }
+            else
+            {
+                if (facing != 'idle')
+                {
+                    player.animations.stop();
+
+                    if (facing == 'left')
+                    {
+                        player.frame = 0;
+                    }
+                    else
+                    {
+                        player.frame = 5;
+                    }
+
+                    facing = 'idle';
+                }
+            }
+
+            if (jumpButton.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+            {
+                player.body.velocity.y = -250;
+                jumpTimer = game.time.now + 750;
+            }
     };
 };
