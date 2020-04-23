04f80389c0dfe5602968360a0a714d7b4d1cccae
midband <augustgw@gmail.com>
2020-04-22 21:10:12 -0400

    HW6 upload

---
+++ HW6/js/boot.js
@@ -0,0 +1,36 @@
+var BootScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+
+    preload: function ()
+    {  
+        this.load.audio('music', 'assets/Go lins 2.mp3');
+        // map tiles
+        this.load.image('tiles', 'assets/map/spritesheet.png');
+        this.load.image('colored', 'assets/battleground/colored.png');
+        
+        // map in json format
+        this.load.tilemapTiledJSON('map', 'assets/map/map.json');
+        this.load.tilemapTiledJSON('battleground', 'assets/battleground/battleground.json');
+        
+        // enemies
+        this.load.image("dragonblue", "assets/dragonblue.png");
+        this.load.image("dragonorrange", "assets/dragonorrange.png");
+        
+        // our two characters
+        this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+    },
+
+    create: function ()
+    {
+        // start the WorldScene
+        this.scene.start('WorldScene');
+    }
+});
---
+++ HW6/js/main.js
@@ -0,0 +1,22 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 768,
+    height: 384,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false // set to true to view zones
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene,
+        BattleScene,
+        UIScene
+    ]
+};
+var game = new Phaser.Game(config);
